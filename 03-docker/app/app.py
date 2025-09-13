60e23cb5cc748e2d0d2a12fcf52ced5e6b95574b
unknown <r3xxuu@gmail.com>
2025-09-13 14:09:33 +0300

    update]

--- 03-docker/app/app.py
+++ 03-docker/app/app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, Flask from Jenkins!"
+def home():
+    # Read variables from environment
+    app_message = os.getenv("APP_MESSAGE", "Default message")
+    db_password = os.getenv("DB_PASSWORD", "No password set")
+
+    return f"""
+    <h1>{app_message}</h1>
+    <p>Database password: {db_password}</p>
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
